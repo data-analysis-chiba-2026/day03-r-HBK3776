3df6deb2eeed8cd26325e10330d8c482d48c05c6
HBK3776 <25zb1040@student.gs.chiba-u.jp>
2026-06-26 16:47:56 +0900

    Mine name ist Masaaki Habuka.

--- day03_homework.R
+++ day03_homework.R
@@ -1,6 +1,6 @@
 # DAY 3 HOMEWORK
 #
-# Name: Joel Nitta
+# Name: Masaaki Habuka
 
 # 0. This demonstrates how to save your answers.
 # I've written the answer for you.
@@ -28,11 +28,19 @@ answer_0 <- 365 * 5
 weight_lb <- 160
 height_ft <- 6
 
+weight_kg <- weight_lb * 0.4536
+height_m <- height_ft * 0.3048
+
+answer_1 <- weight_kg / (height_m^2)
+
+
 # 2. The following code has an error. Fix it by editing the code.
 # (You don't need to save a separate answer for this one, just fix the code.)
 
 volume_oz <- 130
-volume_ml <- Volume_oz * 29.574
+volume_ml <- volume_oz * 29.574
+#comment. Rstudio distinguishes whether capital or not.
+
 
 # 3. What is the value of z after running the following code?
 #
@@ -43,11 +51,18 @@ y <- 3
 z <- x + y
 x <- 5
 
+answer_3 <- 5
+
+
 # 4. The sqrt() function calculates the square root of a number.
 #
 # Use the sqrt() function to calculate the square root of 200, and
 # save your answer in an object called answer_4.
 
+answer_4 <- sqrt(200)
+  
+  
+  
 # 5. The seq() function generates sequences of numbers.
 # Its arguments include "from" (the starting number), "to"
 # (the ending number), and "by" (how much to add
@@ -55,6 +70,8 @@ x <- 5
 #
 # Use seq() make a sequence of even numbers starting at 2 and ending at 100, and
 # save your answer in an object called answer_5
+
+answer_5 <- seq(2,100)
 
 # 6. Make a vector of logical values (TRUE and FALSE), and save it to an
 #  object called lgl_vec
@@ -69,6 +86,12 @@ x <- 5
 # HINT: use c() to make vectors
 # HINT: characters are always written with quotation marks
 
+lgl_vec <- c(TRUE, FALSE)
+chr_vec <- c("LAS", "Chiba-u")
+combine <- c(lgl_vec, chr_vec)
+
+answer_6 <- typeof(combine)
+
 # 7. R includes some pre-defined vectors, like `letters`:
 letters
 
@@ -78,3 +101,7 @@ letters
 # Save your answer in an object called answer_7
 #
 # HINT: Use `seq()` inside the square brackets
+
+q7 <- seq(from = 1, to = length(letters), by = 2)
+answer_7 <- letters[q7]
+
